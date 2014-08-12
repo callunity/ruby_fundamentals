58fe4d28fa7d6453f28c7903b7e6edaaceda5849
callunity <heather.e.armstrong@gmail.com>
2014-08-12 12:21:59 -0400

    Finished exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,9 @@
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.capitalize!
 puts "Hi #{name}!"
+puts
+puts "How old are you?"
+age = gets.chomp
+puts "If you've already had a birthday this year, then you were born in #{2014 - age.to_i}."
+puts "If your birthday is later this year, you must have been born in #{2014 - age.to_i - 1}."
 
